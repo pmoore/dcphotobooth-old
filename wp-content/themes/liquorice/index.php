923d8f9ec7d98c6d6f104121b20b6310f39f0cf3
Paul Moore <pmmoor1@gmail.com>
2012-01-25 17:29:42 -0500

    updates to styling and social arrow

--- wp-content/themes/liquorice/index.php
+++ wp-content/themes/liquorice/index.php
@@ -4,7 +4,7 @@
 ?>
   
   
-  <div id="primaryContent">
+  <div id="primaryContent" class="index">
       <img src="wp-content/themes/liquorice/images/like-arrow.png" id="social-like-arrow">
       <img src="wp-content/themes/liquorice/images/booth/stock-dark-2-small.png" class="stock-img-2">
       <img src="wp-content/themes/liquorice/images/booth/stock-dark-small.png" class="stock-img">
