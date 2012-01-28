61b2dffaf0f010b37833142d926b7267961eaa04
Paul Moore <pmmoor1@gmail.com>
2012-01-28 18:52:47 -0500

    Moved large images out, updated migration sql

--- get_dump.sh
+++ get_dump.sh
@@ -1,3 +1,3 @@
-mysqldump --user=root --password=rootpass dcphotobooth_db wp_posts > pages.sql
+mysqldump --user=root --password=rootpass dcphotobooth_db --tables wp_postmeta wp_posts > pages.sql
 perl -pi -w -e 's/local.dcphotobooths.com:8082/dcphotobooth.elimenohp.com/g;' pages.sql
 perl -pi -w -e 's/local.elimenohp.com:8082/dcphotobooth.elimenohp.com/g;' pages.sql
