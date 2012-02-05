03ed6feba409ad1c7c29949b50fa630567d63aa3
Paul Moore <pmmoor1@gmail.com>
2012-02-05 14:04:46 -0500

    Updated blog styling and layout

--- wp-content/themes/liquorice/blog-bckup.php
+++ wp-content/themes/liquorice/blog-bckup.php
@@ -24,12 +24,13 @@ while ($wp_query->have_posts()) : $wp_query->the_post();
 
             <h2 class="postTitle"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
-            <div class="date" id="post-date"><small><?php the_time('M j') ?></small></div>
+            <div style="text-align: right"><p class="date"><small><?php the_time('F j, Y'); ?></small></p></div>
+            <div class="date" id="post-date"><small><?php the_time('M j') ?><br /><?php the_time('Y') ?></small></div>
 
             <div class="post">
-                <?php //the_content(__('(more...)')); ?>
-                <?php echo get_post_meta(get_the_ID(), 'teaser', true); ?>
-            </div>
+                <div id="postthmb"><?php the_post_thumbnail(); ?></div>
+
+                <?php the_content(__('(more...)')); ?></div>
                 <?php wp_link_pages('before=<p class="page-link">&after=</p>&next_or_number=number&pagelink=page %'); ?>
             <p class="postMeta">Category <?php the_category(', ') ?> | Tags: <?php the_tags(', '); ?> | <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></p>
 
