d2da4dbe693d00d3cc913c02c88c52689598e63e
Paul Moore <pmoore@weddingwire.com>
2012-02-21 11:47:45 -0500

    moved GA from header to aplpication js to make sure it does not cache

--- wp-content/themes/liquorice/header.php
+++ wp-content/themes/liquorice/header.php
@@ -41,21 +41,6 @@
 	if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
   ?>
     
-    <!--    GOOGLE ANALYTICS-->
-    <script type="text/javascript">
-
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-29355521-1']);
-        _gaq.push(['_trackPageview']);
-
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-
-    </script>
-    
 </head>
 
     <body <?php body_class(); ?> id="<?php echo $pagename ?>" >
