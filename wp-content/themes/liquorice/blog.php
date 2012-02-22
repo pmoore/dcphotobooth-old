3a27a013cc7468c7412f213782f605e6dfac755b
Paul Moore <pmoore@weddingwire.com>
2012-02-21 19:16:37 -0500

    Updated pagination images

--- wp-content/themes/liquorice/blog.php
+++ wp-content/themes/liquorice/blog.php
@@ -37,13 +37,15 @@ get_header();
             <div class="post-read-more"><a href="<?php the_permalink() ?>" rel="bookmark">read more &raquo;</a></div>
             <div class="post-social">
                 <!-- AddThis Button BEGIN -->
-                <div class="addthis_toolbox addthis_default_style ">
-                <a class="addthis_button_preferred_1"></a>
-                <a class="addthis_button_preferred_2"></a>
-                <a class="addthis_button_preferred_3"></a>
-                <a class="addthis_button_preferred_4"></a>
-                <a class="addthis_button_compact"></a>
-                <a class="addthis_counter addthis_bubble_style"></a>
+                <div class="addthis_toolbox addthis_default_style "
+                      addthis:url="<?php the_permalink() ?>"
+                      addthis:title="<?php the_title(); ?>">
+                    <a class="addthis_button_preferred_1"></a>
+                    <a class="addthis_button_preferred_2"></a>
+                    <a class="addthis_button_preferred_3"></a>
+                    <a class="addthis_button_preferred_4"></a>
+                    <a class="addthis_button_compact"></a>
+                    <a class="addthis_counter addthis_bubble_style"></a>
                 </div>
                 <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f18708410c8b891"></script>
                 <!-- AddThis Button END -->
