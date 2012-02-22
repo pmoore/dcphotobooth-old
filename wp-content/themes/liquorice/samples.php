9d4549c36a3850d63e6f1b0db1b1a6cf056c031a
Paul Moore <pmoore@weddingwire.com>
2012-02-22 11:53:18 -0500

    Added samples template

---
+++ wp-content/themes/liquorice/samples.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+  Template Name: Samples Page
+ */
+
+ get_header();
+
+?>
+
+<div id="primaryContent">
+    <div class="post">
+        <h1 class="postTitle page">sample galleries</h1>
+        <img class="alignnone size-full wp-image-130 postimg sample-gallery" title="DCPB_0052" src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/DCPB_0052.jpg" alt="" width="160" width="240" />
+        <img src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/DCPB_0011.jpg" alt="" title="DCPB_0011" width="160" width="240" class="alignnone size-full wp-image-133 postimg sample-gallery" />
+        <img src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/DCPB_0021.jpg" alt="" title="DCPB_0021" width="160" width="240" class="alignnone size-full wp-image-137 postimg sample-gallery" />
+        <img src="http://www.dc-photobooth.com/wp-content/uploads/2012/02/all-1-200x300.jpg" alt="Co-founders of DC Photobooth" title="DC Photobooth" width="160" height="240" class="alignnone size-medium wp-image-114 postimg sample-gallery" />
+        <img src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/DCPB_0010-21.jpg" alt="" title="DCPB_0010 (2)" width="160" width="240" class="alignnone size-full wp-image-141 postimg sample-gallery far-right" />
+        <img src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/stock-dark.png" alt="" title="stock-dark" width="160" height="240" class="alignnone size-full wp-image-145 postimg sample-gallery" />
+        <img src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/stock-dark-4.png" alt="" title="stock-dark-4" width="160" height="240" class="alignnone size-full wp-image-146 postimg sample-gallery" />
+        <img src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/guestbook-2.jpeg" alt="" title="guestbook-2" width="160" height="240" class="alignnone size-full wp-image-148 postimg sample-gallery" />
+        <img src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/stock-dark-5.png" alt="" title="stock-dark-5" width="160" height="240" class="alignnone size-full wp-image-147 postimg sample-gallery" />
+        <img src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/stock-dark-2.png" alt="" title="stock-dark-2" width="160" height="240" class="alignnone size-full wp-image-149 postimg sample-gallery far-right" />
+        <div id="main-image-dialog"><img src="http://www.dc-photobooth.com/wp-content/uploads/2012/01/DCPB_0052.jpg" id="main-sample-image"><br/>click anywhere outside image to close</div>
+    </div>
+</div>
+
+
+<?php
+get_footer();
+?>
