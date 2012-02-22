27b3c9ac5794ee40616e0952c4f3c3b722b124df
Paul Moore <pmoore@weddingwire.com>
2012-02-21 19:22:18 -0500

    Updated pagination images

--- wp-content/themes/liquorice/blog.php
+++ wp-content/themes/liquorice/blog.php
@@ -40,10 +40,10 @@ get_header();
                 <div class="addthis_toolbox addthis_default_style "
                       addthis:url="<?php the_permalink() ?>"
                       addthis:title="<?php the_title(); ?>">
-                    <a class="addthis_button_preferred_1"></a>
-                    <a class="addthis_button_preferred_2"></a>
-                    <a class="addthis_button_preferred_3"></a>
-                    <a class="addthis_button_preferred_4"></a>
+                    <a class="addthis_button_facebook"></a>
+                    <a class="addthis_button_twitter"></a>
+                    <a class="addthis_button_gmail"></a>
+                    <a class=""></a>
                     <a class="addthis_button_compact"></a>
                     <a class="addthis_counter addthis_bubble_style"></a>
                 </div>
