035b3e536b1438f045fb5d86d881e6468d85f8aa
Paul Moore <pmoore@weddingwire.com>
2012-02-23 10:18:43 -0500

    added pinterest button to blog posts

--- wp-content/themes/liquorice/single.php
+++ wp-content/themes/liquorice/single.php
@@ -15,10 +15,10 @@
       <div class="date"><small><?php the_date(); ?> by <?php the_author(); ?></small></div>
       <!-- AddThis Button BEGIN -->
         <div class="addthis_toolbox addthis_default_style">
+            <a class="addthis_button_pinterest" pi:pinit:url="<?php the_permalink() ?>" pi:pinit:media="" pi:pinit:layout="horizontal"></a>
             <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
             <a class="addthis_button_tweet"></a>
             <a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
-            <a class="addthis_counter addthis_pill_style"></a>
         </div>
         <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f18708410c8b891"></script>
         <!-- AddThis Button END -->
