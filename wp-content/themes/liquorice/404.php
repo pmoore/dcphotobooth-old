a13534dac28d621bd17106dfbf5b6e103c28f738
Paul Moore <pmoore@weddingwire.com>
2012-03-02 10:37:16 -0500

    updated 404

--- wp-content/themes/liquorice/404.php
+++ wp-content/themes/liquorice/404.php
@@ -3,12 +3,12 @@
 
 <div id="content" class="error-page">
  <h2><span>404</span> <br /> 
- <img src="http://test.dcphotobooths.com:8085/wp-content/uploads/2012/02/paulmoore-150x150.jpg" alt="Paul Moore - Supreme Emporer of DC Photobooth" title="Paul Moore" width="200" height="200" class="size-thumbnail wp-image-106 postimg memberimg"><br />    
- Nonexistent Area</h2>
+ <img src="/wp-content/uploads/2012/02/paulmoore-200x300.jpg" alt="Paul Moore - Supreme Emporer of DC Photobooth" title="Paul Moore" width="200" height="300" class="size-thumbnail wp-image-106 postimg memberimg"><br />    
+ What'cha lookin for?</h2>
  
  
  
- <p><strong>Oops!  Not sure what you were looking for, but it doesn't seem to be here!</strong><br />
+ <p><strong>We're not sure, but it doesn't seem to be here!</strong><br />
 </p>
 <dl>
 	<dt><a href="<?php echo home_url(); ?>">Let's head back home, shall we?</a></dt>
