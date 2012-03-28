f70238e3b4502a560969f67c4ee041482e69ed95
Paul Moore <pmmoor1@gmail.com>
2012-03-27 21:20:29 -0400

    styled event page more

--- wp-content/themes/liquorice/event-photos.php
+++ wp-content/themes/liquorice/event-photos.php
@@ -12,21 +12,24 @@
                             'date' => date("M d, Y", strtotime("2012-01-26")), 
                             'featured_image' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/03/20000101_000347-201x300.jpg',
                             'featured_image_large' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/03/20000101_000347.jpg',
-                            'host_url' => 'http://dc-photobooth.smugmug.com/Weddings/DC-Photobooth-Launch-Party/22144760_cZ9Nqj'
+                            'host_url' => 'http://dc-photobooth.smugmug.com/Weddings/DC-Photobooth-Launch-Party/22144760_cZ9Nqj',
+                            'link_title' => 'DC Photobooth Launch Party'
                         ),               
                     array(  'name' => 'Baltimore Sheraton North Hotel', 
                             'type' => 'Bridal Show', 
                             'date' => date("M d, Y", strtotime("2012-02-19")), 
                             'featured_image' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/02/dcphotobooth10-201x300.jpg',
                             'featured_image_large' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/02/dcphotobooth10.jpg',
-                            'host_url' => 'http://dc-photobooth.smugmug.com/Weddings/DC-Photobooth/21522481_Hc6Zp9/'
+                            'host_url' => 'http://dc-photobooth.smugmug.com/Weddings/DC-Photobooth/21522481_Hc6Zp9/',
+                            'link_title' => 'DC Photobooth at the Baltimore Sheraton North Bridal Show'
                         ),
-                    array(  'name' => 'Truong Wedding', 
+                    array(  'name' => 'Tai & Becci Truong', 
                             'type' => 'Wedding', 
                             'date' => date("M d, Y", strtotime("2012-03-24")), 
                             'featured_image' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/03/20000101_011205-201x300.jpg',
                             'featured_image_large' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/03/20000101_011205.jpg',
-                            'host_url' => 'http://dc-photobooth.smugmug.com/Weddings/3-24-Truong-Wedding/22108372_DKwP2T'
+                            'host_url' => 'http://dc-photobooth.smugmug.com/Weddings/3-24-Truong-Wedding/22108372_DKwP2T',
+                            'link_title' => 'DC Photobooth at the Wedding of Tai and Becci Truong'
                         )
             );
  
@@ -61,7 +64,12 @@
         
         <div id="temp-image-filler" style="float:right;">
             <?php foreach ( $events as $key=>$event ) { ?>
-                <img src="<?php echo $event['featured_image_large'] ?>" width="375" height="562" class="postimg">
+                <div>
+                    <img src="<?php echo $event['featured_image_large'] ?>" width="375" height="562" class="postimg">
+                    <div class="event-item-name" style="width:385px; margin-top: 10px;"><?php echo $event['name'] ?></div>
+                    <span class="event-item-date" style="width:385px;"><?php echo $event['date'] ?></span>
+                    <span class="event-item-type" style="width:385px;"><?php echo $event['type'] ?></span>
+                </div>
             <?php } ?>
         </div>
         
