a80b9ecf0875f9cf995ad76a15872bd250c61c74
Paul Moore <pmoore@weddingwire.com>
2012-03-30 11:24:00 -0400

    changed pinterest loading for blog images

--- wp-includes/js/application.js
+++ wp-includes/js/application.js
@@ -134,36 +134,58 @@ $(function(){
     
     
     /* Pinterest button on hover of postimgs */
-    $('.postimg').each(function(index){ 
+    $('.post .postimg').each(function(index){ 
         /*Place pinterest button on img*/
-        $(this).data('pinterest_btn',$('#social_toolbox').find('a.addthis_button_pinterest').clone());
-        var p = $(this).position();
-        $(this).data('pinterest_btn').css({
-            display: 'none',
-            position: 'absolute',
-            top: p.top+15,
-            left: p.left+10
-        });  
-        $(this).data('pinterest_btn').attr('pi:pinit:media',$(this).attr('src'));        
-        $(this).closest('.post').append($(this).data('pinterest_btn'));
-        
+//        $(this).data('pinterest_btn',$('#social_toolbox').find('a.addthis_button_pinterest').clone());
+        var p = $(this).offset();
+//        $(this).data('pinterest_btn').css({
+//            display: 'none',
+//            position: 'absolute',
+//            top: p.top+15,
+//            left: p.left+10
+//        });  
+//        $(this).data('pinterest_btn').attr('pi:pinit:media',$(this).attr('src'));        
+//        $(this).closest('.post').append($(this).data('pinterest_btn'));
+//        
+//        $(this).mouseover(
+//            function(){    
+//                clearTimeout($(this).data('pinterest_btn').data('pinhide_timeout'+index));
+//                $(this).data('pinterest_btn').fadeIn(400);
+//        }).mouseout(
+//            function(){
+//                pin_btn = $(this).data('pinterest_btn');
+//                var pinhide_timeout = setTimeout(function(){pin_btn.fadeOut(400)}, 100);
+//                $(this).data('pinterest_btn').data('pinhide_timeout'+index, pinhide_timeout);
+//        });
+//        $(this).data('pinterest_btn').mouseover(
+//            function(){
+//                clearTimeout($(this).data('pinhide_timeout'+index));
+//            }).mouseout(
+//            function(){
+//                //mouse out handler
+//        });
+//      
+        var pinit_html = '<div style="display: none; position: absolute; top: '+(p.top-113)+'px; left: '+(p.left-200)+'px;" id="pinit-container-'+index+'"><a class="addthis_button_pinterest" pi:pinit:url="http://www.dc-photobooth.com'+ window.location.pathname +'" pi:pinit:media="' + $(this).attr('src') + '" pi:pinit:layout="horizontal" style="outline:none;"></a></div>';
+       
+        $(this).closest('.post').append(pinit_html);
+
         $(this).mouseover(
-            function(){    
-                clearTimeout($(this).data('pinterest_btn').data('pinhide_timeout'+index));
-                $(this).data('pinterest_btn').fadeIn(400);
-        }).mouseout(
             function(){
-                pin_btn = $(this).data('pinterest_btn');
-                var pinhide_timeout = setTimeout(function(){pin_btn.fadeOut(400)}, 100);
-                $(this).data('pinterest_btn').data('pinhide_timeout'+index, pinhide_timeout);
-        });
-        $(this).data('pinterest_btn').mouseover(
-            function(){
-                clearTimeout($(this).data('pinhide_timeout'+index));
+                clearTimeout($('#pinit-container-'+index).data('pinhide_timeout'+index));
+                $('#pinit-container-'+index).fadeIn(400);
             }).mouseout(
             function(){
-                //mouse out handler
-        });
+                var pinhide_timeout = setTimeout(function(){$('#pinit-container-'+index).fadeOut(400)}, 400);
+                $('#pinit-container-'+index).data('pinhide_timeout'+index, pinhide_timeout);
+            });
+        $('#pinit-container-'+index).hover(
+            function(){
+                console.log("Pin hover in");
+                clearTimeout($(this).data('pinhide_timeout'+index));
+            },
+            function(){
+//                $('#pinit-container-'+index).fadeOut(400);
+            });
     });
     
     
