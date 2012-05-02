4802dcd7705b56d534026689e077e24fa3685748
Paul Moore <pmmoor1@gmail.com>
2012-05-02 19:33:53 -0400

    Added WW recruitment event

--- wp-content/themes/liquorice/event-photos.php
+++ wp-content/themes/liquorice/event-photos.php
@@ -7,13 +7,13 @@
  get_header();
  
  $events = array (  
-                    array(  'name' => 'Colleen & Ryan Hill', 
-                            'type' => 'Wedding', 
-                            'date' => date("M d, Y", strtotime("2012-04-28")), 
-                            'featured_image' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/05/20000101_015647-201x300.jpg',
-                            'featured_image_large' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/05/20000101_015647.jpg',
-                            'host_url' => 'http://dc-photobooth.smugmug.com/Weddings/4-28-Colleen-and-Ryan-Wedding/22763435_WhXcmT',
-                            'link_title' => 'DC Photobooth at the Wedding of Colleen and Ryan Hill'
+                    array(  'name' => 'WeddingWire Recruitment Event', 
+                            'type' => 'Recruitment', 
+                            'date' => date("M d, Y", strtotime("2012-04-30")), 
+                            'featured_image' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/05/20000101_034046-201x300.jpg',
+                            'featured_image_large' => 'http://www.dc-photobooth.com/wp-content/uploads/2012/05/20000101_034046.jpg',
+                            'host_url' => 'http://dc-photobooth.smugmug.com/Professional/4-30-WeddingWire-Recruiting/22763467_3Sj6Z7',
+                            'link_title' => 'DC Photobooth at the a WeddingWire Recruitment Event at the Majestic'
                         ),
                     array(  'name' => 'NBAC Countdown to the Olympics', 
                             'type' => 'Fundraiser', 
