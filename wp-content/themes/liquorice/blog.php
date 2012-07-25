6980367d24989d06f1b093df935fde6608cb6e34
Paul Moore <pmoore@weddingwire.com>
2012-07-25 12:12:10 -0400

    increased blog posts per page

--- wp-content/themes/liquorice/blog.php
+++ wp-content/themes/liquorice/blog.php
@@ -15,7 +15,7 @@ get_header();
         $temp = $wp_query;
         $wp_query = null;
         $wp_query = new WP_Query();
-        $wp_query->query('posts_per_page=5' . '&paged=' . $paged);
+        $wp_query->query('posts_per_page=10' . '&paged=' . $paged);
         while ($wp_query->have_posts()) : $wp_query->the_post();
         ?>
 
