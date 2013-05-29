070c9c7f934847c57bb81b489b9bfe4b32eae5bb
pmoore <pmoore@weddingwire.com>
2013-05-29 09:52:48 -0400

    couple events

--- wp-content/themes/liquorice/event-photos.php
+++ wp-content/themes/liquorice/event-photos.php
@@ -7,7 +7,23 @@
 get_header();
 
  
- $events = array (  array(  'name' => 'Susan & Tom',  
+ $events = array (  array(  'name' => 'Marie & Jon',  
+                            'type' => 'Wedding', 
+                            'date' => date("M d, Y", strtotime("2013-05-26")), 
+                            'featured_image' => 'http://www.dc-photobooth.com/wp-content/uploads/2013/05/5.26-Marie-Jon-201x300.jpg',
+                            'featured_image_large' => 'http://www.dc-photobooth.com/wp-content/uploads/2013/05/5.26-Marie-Jon.jpg',
+                            'host_url' => 'http://dc-photobooth.smugmug.com/Weddings/Marie-and-Jon/29676178_42Bzzq',
+                            'link_title' => 'DC Photobooth at Marie and Jon\'s Wedding'
+                        ),
+                    array(  'name' => 'Randy\'s Graduation',  
+                            'type' => 'Graduation Party', 
+                            'date' => date("M d, Y", strtotime("2013-05-18")), 
+                            'featured_image' => 'http://www.dc-photobooth.com/wp-content/uploads/2013/05/5.18-Randys-Graduation-201x300.jpg',
+                            'featured_image_large' => 'http://www.dc-photobooth.com/wp-content/uploads/2013/05/5.18-Randys-Graduation.jpg',
+                            'host_url' => 'http://dc-photobooth.smugmug.com/Other/Randys-Graduation/29676156_sW9Zjv',
+                            'link_title' => 'DC Photobooth at Randy\'s Graduation Party'
+                        ),
+                    array(  'name' => 'Susan & Tom',  
                             'type' => 'Wedding', 
                             'date' => date("M d, Y", strtotime("2013-05-11")), 
                             'featured_image' => 'http://www.dc-photobooth.com/wp-content/uploads/2013/05/5.11-susan-tom-201x300.jpg',
