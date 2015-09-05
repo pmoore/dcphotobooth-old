4aaed33b53c6c99c5520b350db7bc170e7bf4f22
pmoore <pmmoor1@gmail.com>
2015-09-05 19:23:06 -0400

    Changing lead form to use 17hats lead capture

--- wp-content/themes/liquorice/contact.php
+++ wp-content/themes/liquorice/contact.php
@@ -12,48 +12,10 @@ get_header();
         <h1 class="postTitle page">send us a message!</h1>
         
         <div style="clear:both"></div>
-        <div id="contact-form-container"><form id="contact-form" class="contact-form" action=" " method="post">
-                <div id="contact-form-left">
-                    <label class="iconic user" for="username"> First &amp; Last Name <span class="required">*</span></label><br />
-                    <input id="username" type="text" name="username" placeholder="What can we call you?"/>
-                    <br />
-                    <label class="iconic mail-alt" for="usermail"> E-mail address <span class="required">*</span></label><br/>
-                    <input id="usermail" type="email" name="usermail" placeholder="Trust me, we hate spam as much as you do." />
-                    <br />
-                    <label class="iconic location" for="userlocation"> Location </label><br />
-                    <input id="userlocation" type="text" name="userlocation" placeholder="Where is this all going down?"/>
-                    <br />
-                    <label class="iconic date" for="userdate"> Event Date </label><br />
-                    <input id="userdate" type="text" name="userdate" placeholder="...and when?"/>
+        <div id="contact-form-container">
+            <iframe name="lc_contact_form" frameborder="0" width="100%" height="600" src="https://70981.17hats.com/embed/lead/form/vgbtnckkwcnncptwcfhpkwwsdcxcddvf"></iframe><script type="text/javascript" src="https://70981.17hats.com/js/lib/iframeSizer.min.js"></script>
 
-                </div>
-                <div id="contact-form-right"><label class="iconic quote-alt" for="subject"> Subject </label><br />
-                    <input id="subject" type="text" name="subject" placeholder="What's this all about?" />
-                    <br />
-                    <label class="iconic comment" for="message"> Message <span class="required">*</span></label><br />
-                    <textarea placeholder="Don't be shy!  Tell us a little bit about your event, what you're looking for from a photobooth company, or just be nice and say hi!" id="message"></textarea>
-
-                </div>
-                <div style="clear: both;"></div>
-                
-                <label class="iconic referrer" for="userdate"> How did you find out about us? </label><br />
-                <div class="style-select">
-                    <select id="referrer">
-                        <option value="Google">Google</option>
-                        <option value="Yelp">Yelp</option>
-                        <option value="Facebook">Facebook</option>
-                        <option value="WeddingWire">WeddingWire</option>
-                        <option value="Craig's List">Craigslist</option>
-                        <option value="From a Friend">From a Friend</option>
-                        <option value="Other">Other</option>
-                    </select>
-                </div>
-
-                
-                <p class="indication">All fields with a <span class="required">*</span> are required</p>
-                <input type="submit" value="Send your message !" />
-
-            </form></div>
+        </div>
 
     </div>
     <div class="italic" style="text-align: center; color: #666;">
